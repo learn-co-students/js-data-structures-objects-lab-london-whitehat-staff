83c77e6e7a673ebe6f3bc0b7ebf29b313ce43a0c
Kelly <kelly.ho@sky.uk>
2019-03-20 15:12:11 +0000

    Done.

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   })
 }
 
+//drive.name = sam is defined inside the test. We then only have to add in one [key]: value hence that^.
+
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  driver.name = 'Sam';
-  driver.address ='12 Broadway';
+  driver[key] = value;
   return driver;
 }
+
+//it is getting value from the test. driver[key] is saying look for the key (which = address, links to previous test), then take the value assigned to that key.
 
 function deleteFromDriverByKey(driver, name) {
   let newdriver = Object.assign({}, driver)
